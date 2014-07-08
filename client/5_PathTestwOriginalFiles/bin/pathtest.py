b932e68cc5ded05a2da4a0d3a90812bf9cbe9f1c
Reed S <rshin808@yahoo.com>
2014-07-08 13:50:21 -1000

    implemented class structures, not really done yet

---
+++ client/5_PathTestwOriginalFiles/bin/pathtest.py
@@ -0,0 +1,16 @@
+import os
+
+script_directory = os.path.dirname(os.path.abspath(__file__))
+script_directory_list = str(script_directory).split("\\")
+files_directory_list = script_directory_list[:-1]
+files_directory_list.append("Data Files")
+files_directory = "\\".join(files_directory_list)
+files = os.listdir(files_directory)
+
+print "\n"
+print "Script Directory: ", script_directory
+print "Files in Script Directory: ", os.listdir(script_directory)
+print "\n"
+print "Files Directory: ", files_directory
+print "Files in Files Directory: ", os.listdir(files_directory)
+print "\n" 
